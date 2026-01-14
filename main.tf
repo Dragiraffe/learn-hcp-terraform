19a113dd19743c6cfa14794d8629f996d14ae398
Adrienn Szocs <adrienn.szocs@gmail.com>
2026-01-14 18:11:43 +0100

    update outputs file 2

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_network_security_group" "rg" {
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "myTFVnet"
+  name                = "myNewTFVnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
